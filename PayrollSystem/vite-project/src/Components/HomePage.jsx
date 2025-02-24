f038ca0682965a7dde1fd47df661039f759c8ae0
shairahkaye <shairahkayecabal@gmail.com>
2025-02-24 08:47:28 +0800

    react-vite

---
+++ PayrollSystem/vite-project/src/Components/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page!</h1>
+        <p>This is a component from the components folder.</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
